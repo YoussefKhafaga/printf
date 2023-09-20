2934a3cdbc79d98a677875dc230b3577eaf79f5b
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-20 06:30:25 -0700

    20923

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
-#ifndef HEADERFILE
-#define HEADERFILE
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 void free_2D(char **array, int n);
@@ -12,7 +14,6 @@ char *_memset(char *s, char b, unsigned int n);
 unsigned int get_char(char c, unsigned int len);
 unsigned int get_string(char *str_format, unsigned int len);
 unsigned int get_percent(unsigned int len);
-int _printf(const char *format, ...);
-int _strlen(char *str);
-int print_string(char *str);
+
 #endif
+
